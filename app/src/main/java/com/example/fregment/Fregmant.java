81d8fdd25837e3be448d99cd0dd3e89395efd627
Abdelrahman <abdokh950@gmail.com>
2021-05-11 00:40:44 +0200

    fixed layouts

--- app/src/main/java/com/example/fregment/Fregmant.java
+++ app/src/main/java/com/example/fregment/Fregmant.java
@@ -2,19 +2,33 @@ package com.example.fregment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Fregmant extends Fragment {
 
-
+    static int ColorBound =16777216;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fregment_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView=(TextView) view.findViewById(R.id.textView5);
+        Random rand = new Random();
+        int nextColor = rand.nextInt(ColorBound);
+        textView.setBackgroundColor(nextColor);
     }
 }
